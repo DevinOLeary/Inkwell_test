74f1f0f21bca72b69f832edf3c8c5e33fe0d6e63
Devin <djoleary07@gmail.com>
2018-02-19 16:06:35 -0500

    src update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,90 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import {throttle} from 'lodash';
 
-import Header from '../components/Header'
-import './index.css'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
+
+
+import '../stylesheets/index.scss'
+
+class TemplateWrapper extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.state = {
+      menuShow: false
+    }
+  }
+  componentDidMount(){
+    window.addEventListener('scroll', throttle(this.handleScroll, 200));
+  }
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(){
+    const scrollTop = window.scrollY;
+    const offSet = this.body.offsetTop;
+    const distance = (offSet - scrollTop);
+    if(distance < -450){
+      this.setState({menuShow: true});
+    } else {
+      this.setState({menuShow: false});
+    }
+  }
+
+
+  render(){
+    const visible = this.state.menuShow;
+    return (
+      <div>
+        <Helmet
+          title={this.props.data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Located in Bloomington, IN, the Inkwell Cafe uses great food, drinks, personality, and aesthetics to create the prefect atmophere to unwind and treat yourself to something great.' },
+            { name: 'keywords', content: 'cafe, coffee, breakfast, local, atmosphere, espresso, bakery' },
+          ]}
+        />
+        {/* display header when not at root url */}
+        {this.props.location.pathname !== "/" ?
+          <Header data={this.props.data} location={this.props.location}/>
+          :
+          <div/>
+        }
+          <div className={`below-fold-nav ${visible ? 'show-nav' : ''}`}>
+            <Header data={this.props.data} location={this.props.location}/>
+          </div>
+
+        <div ref={(body) => {this.body = body; }}>
+          {this.props.children()}
+        </div>
+
+        {this.props.location.pathname !== "/" ?
+          <Footer data={this.props.data} screenSize={this.state.screenSize}/>
+          :
+          <div/>
+        }
+      </div>
+    );
+  }
+}
+
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
 }
 
 export default TemplateWrapper
+
+export const query = graphql `
+  query titleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+import Animations from '../components/Animations';
+import Waypoint from 'react-waypoint';
+
+const backgroundImage = {
+  backgroundColor: 'black',
+  height: '100vh'
+}
+
+const hidden = {
+  opacity: 0
+}
+
+const IndexPage = ({data}) => (
+  <Waypoint onEnter={Animations.animateListIn.bind(this, "landingContainer")}>
+    <ul className="container column" style={backgroundImage} id="landingContainer">
+      <li style={hidden}><h2 className="inverse">Welcome to {data.site.siteMetadata.title}</h2></li>
+      <li style={hidden}><Link to="/home/"><button className="inverse-button"><h4>check it</h4></button></Link></li>
+    </ul>
+  </Waypoint>
 )
 
 export default IndexPage
+
+export const query = graphql `
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
